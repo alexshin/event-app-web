1e771517b22b096c3aeeeed3b9be50e40c9be3a4
Alex Shinkevich <a.shinkevich@ittopgroup.com>
2018-10-29 20:11:10 +0300

    Added request to fetch platform info

--- src/services/requests.js
+++ src/services/requests.js
@@ -1,6 +1,7 @@
 import {httpClient, getAuthHeader} from './httpClient';
-
+import * as settings from '../settings';
 
 export const getPlatformInfo = async () => {
-    const data = httpClient.get('')
+    const resp = await httpClient.get(settings.PLATFORM_INFO_URI);
+    return resp.data;
 };
